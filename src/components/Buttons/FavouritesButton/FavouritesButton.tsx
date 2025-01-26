b1a922371d2b5e6ee582af17cb3840331c4d463f
elencodes <playnonstop205@gmail.com>
2025-01-26 18:33:17 +0400

    fixed the favorites button component (added new styles)

--- src/components/Buttons/FavouritesButton/FavouritesButton.tsx
+++ src/components/Buttons/FavouritesButton/FavouritesButton.tsx
@@ -1,43 +1,24 @@
 import { useState } from "react";
-import favouritesIcon from "../../../assets/icons/buttons/favourite.svg";
-import favouritesActiveIcon from "../../../assets/icons/buttons/favourite-active.svg";
 import styles from "./FavouritesButton.module.scss";
 
 const FavouritesButton = () => {
-	const [isFavourite, setIsFavourite] = useState(false); //Состояние добавления в "избранное"
-	const [isHovered, setIsHovered] = useState(false); //Состояние hover
+	const [isFavourite, setIsFavourite] = useState(false);
 
 	const toggleFavourite = () => {
 		setIsFavourite((prev) => !prev);
 	};
 
 	return (
-		<>
-			<button
-				className={styles.button__favourites}
-				onClick={toggleFavourite}
-				onMouseEnter={() => setIsHovered(true)}
-				onMouseLeave={() => setIsHovered(false)}
-				aria-label="Добавить в избранное"
-			>
-				<img
-					className={`${styles.button__favourites_icon} ${styles.default}`}
-					src={favouritesIcon}
-					alt="favourites"
-				/>
-				<img
-					className={`${styles.button__favourites_icon} ${styles.active} ${
-						isFavourite || isHovered ? styles.visible : ""
-					}`}
-					src={
-						isFavourite || isHovered
-							? favouritesActiveIcon
-							: favouritesIcon
-					}
-					alt="favourites"
-				/>
-			</button>
-		</>
+		<button
+			className={`${styles.button__favourites} ${
+				isFavourite ? styles.active : ""
+			}`}
+			onClick={toggleFavourite}
+		>
+			<span
+				className={`${styles.heart} ${isFavourite ? styles.active : ""}`}
+			></span>
+		</button>
 	);
 };
 
