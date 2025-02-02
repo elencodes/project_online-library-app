8b7f221846ecfd02aa922cca9b2f418bd25ae943
elencodes <playnonstop205@gmail.com>
2025-02-02 22:36:09 +0400

    fixed store

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,4 +1,5 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
+import { bookReducer } from "./reducers/booksReducer";
 
-export const store = createStore({}, applyMiddleware(thunk)); //инициализация Redux store
+export const store = createStore(bookReducer, applyMiddleware(thunk)); //инициализация Redux store
