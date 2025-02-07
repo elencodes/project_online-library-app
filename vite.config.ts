3b2779cfcaeea457593d27eafd2b0c49dfa52bbd
elencodes <playnonstop205@gmail.com>
2025-02-07 23:22:50 +0400

    fixed vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
 	base: "/project_online-library-app",
 	plugins: [react()],
+	define: {
+		"process.env": {},
+	},
 	css: {
 		preprocessorOptions: {
 			scss: {
-				api: 'modern-compiler',
+				api: "modern-compiler",
 			},
 		},
 	},
-})
+});
