321917e4d300aa0525a08e22ce9e5e5c97722e2b
elencodes <playnonstop205@gmail.com>
2025-02-10 18:33:14 +0400

    fixed FavouritesButton

--- src/components/Buttons/FavouritesButton/FavouritesButton.tsx
+++ src/components/Buttons/FavouritesButton/FavouritesButton.tsx
@@ -1,35 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styles from "./FavouritesButton.module.scss";
 
 const FavouritesButton = () => {
 	const [isFavourite, setIsFavourite] = useState(false);
-	const [disableHover, setDisableHover] = useState(false); // Отключаем `hover` временно
 
 	const toggleFavourite = () => {
 		setIsFavourite((prev) => !prev);
-
-		// Если лайк убрали — отключаем `hover` для FavouritesButton
-		if (isFavourite) {
-			setDisableHover(true);
-			// Включаем hover снова через 0.5 сек
-			setTimeout(() => setDisableHover(false), 500);
-		}
 	};
-
-	useEffect(() => {
-		// Отключаем hover на тач-устройствах
-		const handleTouchStart = () => setDisableHover(true);
-		window.addEventListener("touchstart", handleTouchStart);
-
-		return () => window.removeEventListener("touchstart", handleTouchStart);
-	}, []);
 
 	return (
 		<button
 			key={isFavourite ? "liked" : "unliked"}
 			className={`${styles.button__favourites} ${
 				isFavourite ? styles.active : styles.inactive
-			} ${disableHover ? styles.noHover : ""}`}
+			}`}
 			onClick={toggleFavourite}
 		>
 			<span className={styles.heart}></span>
